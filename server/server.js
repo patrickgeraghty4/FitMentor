eca323a149a5a5c22ce8b9f51f999d2d461f1738
patrickgeraghty4 <patrickgrght@gmail.com>
2025-04-21 22:36:45 -0400

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,23 @@
+// server/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+// MongoDB connection (use your own MongoDB URI in .env file)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Sample route for testing
+app.get('/', (req, res) => {
+  res.send("Welcome to FitMentor API");
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
