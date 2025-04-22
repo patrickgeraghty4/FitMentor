2bbd58f952e4a129e81028db35f49bd7c84c34b3
patrickgeraghty4 <patrickgrght@gmail.com>
2025-04-21 22:15:54 -0400

    Create Header.tsx

---
+++ client/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-500 p-4 text-white">
+      <h1 className="text-xl font-semibold">FitMentor</h1>
+    </header>
+  );
+};
+
+export default Header;
